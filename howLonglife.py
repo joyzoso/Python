0be80d7567e573ba8c4ee064aa517d1de5c578e8
joyzoso <joyrockon@gmail.com>
2015-08-17 10:48:05 -0700

    a small module that prompts a user to enter their age, then calculates age in days, minutes and seconds

--- howLonglife.py
+++ howLonglife.py
@@ -1,4 +1,6 @@
 
+
+
 print("Let's see how long you have lived in days, minutes and seconds,")
 name = input("name: ")
 print("now enter your age")
@@ -7,3 +9,4 @@ days = age * 365
 minutes = age * 525948
 seconds = age * 31556926
 print(name, "has been alive for", days, "days", minutes, "minutes and", seconds, "seconds! Hooray")
+
