328bdc42c3d019aa63924a101baae8aefeefb6d8
joyzoso <joyrockon@gmail.com>
2015-08-20 15:13:37 -0700

    ex20 updated
    updated to add comments

--- ex20.py
+++ ex20.py
@@ -6,11 +6,10 @@ def print_all(f):
 	print f.read()
 
 def rewind(f):
-	f.seek(0) #seek(0) moves the file to the first(0) byte in the file. 
-
+	f.seek(0)
 
 def print_a_line(line_count, f):
-	print line_count, f.readline() #f.readline moves the read head to the right after the /n
+	print line_count	, f.readline()
 
 current_file = open(input_file)
 
@@ -27,7 +26,7 @@ print "Let's print three lines:"
 current_line = 1
 print_a_line(current_line, current_file)
 
-current_line += 1 #this is the same as line 33, just a shorthand way.
+current_line = current_line + 1
 print_a_line(current_line, current_file)
 
 current_line = current_line + 1
