b4e2c815fe2d3d93938bc88374289965aa3f1d34
joyzoso <joyrockon@gmail.com>
2015-08-22 12:37:11 -0700

    ex24.py
    revised

--- LearnPTHW/ex24.py
+++ LearnPTHW/ex24.py
@@ -23,14 +23,14 @@ def secret_formula(started):
 	crates = jars / 100
 	return jelly_beans, jars, crates
 
-	start_point = 10000
-	beans, jars, crates = secret_formula(start_point)
+start_point = 10000
+beans, jars, crates = secret_formula(start_point)
 
-	print "With a starting poitn of: %d" % start_point
-	print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
+print "With a starting point of: %d." % start_point
+print "We'd have %d beans, %d jars, and %d crates." % (beans, jars, crates)
 
-	start_point = start_point / 10
+start_point = start_point / 10
 
-	print "We can also do that this way:"
-	print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
+print "We can also do that this way:"
+print "We'd have %d beans, %d jars, and %d crates." % secret_formula(start_point)
 	
