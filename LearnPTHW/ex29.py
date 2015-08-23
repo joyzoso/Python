d68ce13146d4a0188481a4d382e338249979d1f4
joyzoso <joyrockon@gmail.com>
2015-08-22 20:28:06 -0700

    Ex29
    LPTHW

---
+++ LearnPTHW/ex29.py
@@ -0,0 +1,29 @@
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too many cats! The world is doomed!"
+
+if people > cats:
+	print "Not many cats! Th world is saved!"
+
+if people < dogs:
+	print "The world is drooled on!"
+
+if people > dogs:
+	print "The world is dry!"
+
+dogs += 5
+
+if people >= dogs:
+	print "People are greater then or equal to dogs."
+
+if people <= dogs:
+	print "People are less than or equal to dogs."
+
+if people == dogs:
+	print "People are dogs."
+
+#remember the code X += 1 is the same as doing x = x + 1
+#it is called the "increment by operator". can also be -=							
